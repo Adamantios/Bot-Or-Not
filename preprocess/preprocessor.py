96e2638e05b39e7aa0dd9318de7e76ca2029d263
Adamantios <manos_0_7@hotmail.com>
2016-09-26 17:32:23 +0300

    Make a function wrapper for preprocessing, in order to use it in the sklearn's pipeline.

--- preprocess/preprocessor.py
+++ preprocess/preprocessor.py
@@ -1,4 +1,7 @@
-from preprocess.methods import clean_html, detwittify, remove_numbers, remove_punctuation, remove_links
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from preprocess.methods import clean_html, detweettify, remove_numbers, remove_punctuation, remove_links
+from utilities.file_management import log
 
 
 def preprocess_tweets(tweets):
@@ -6,8 +9,35 @@ def preprocess_tweets(tweets):
 
     tweets -- list - the tweets list to be cleaned."""
     tweets = clean_html(tweets)
-    tweets = detwittify(tweets)
+    tweets = detweettify(tweets)
     tweets = remove_numbers(tweets)
     tweets = remove_punctuation(tweets)
     tweets = remove_links(tweets)
     return tweets
+
+
+# noinspection PyPep8Naming
+class FunctionWrapper(BaseEstimator, TransformerMixin):
+    """ Class to wrap around a function in order to be used in sklearn's pipeline
+
+    Attributes:
+        function : The function to be wrapped around
+    """
+
+    def __init__(self, function):
+        self.function = function
+
+    # noinspection PyUnusedLocal
+    def fit(self, X, y):
+        """ No need to fit"""
+
+        return self
+
+    def transform(self, X):
+        """ Transform data
+
+        X - The input to transform
+        returns - A transformation of X implemented in function
+        """
+        X = self.function(X)
+        return X
